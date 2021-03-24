00d9eddeeb42c4362eb5437e4acb5468254b5852
JairLopesJunior <jair.lopes@fatec.sp.gov.br>
2021-03-24 12:10:13 -0300

    Criado o laço de repetição de opções do Projeto

---
+++ br.com.correforrest/src/br/com/correforrest/Main.java
@@ -0,0 +1,29 @@
+package br.com.correforrest;
+
+import java.util.Scanner;
+
+public class Main {
+	
+	static Scanner scan;
+
+	public static void main(String[] args) {
+		scan = new Scanner(System.in);
+		Integer opcao;
+		
+		do {
+			System.out.println("=======================================================");
+			System.out.println("1.");
+			System.out.println("0.  Sair.");
+			opcao = scan.nextInt();
+			
+			if(opcao == 1) {
+				
+			}
+			
+		}while(opcao != 0); {
+			
+		}
+
+	}
+
+}
