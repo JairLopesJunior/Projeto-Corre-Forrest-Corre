a1ccb4a7c2711dc3f661011e3e21c2ef4665ac3e
JairLopesJunior <jair.lopes@fatec.sp.gov.br>
2021-03-26 12:25:09 -0300

    Criado opção para Editar os dados da Inscrição

--- br.com.correforrest/src/br/com/correforrest/InscricoesCorredores.java
+++ br.com.correforrest/src/br/com/correforrest/InscricoesCorredores.java
@@ -27,10 +27,10 @@ public class InscricoesCorredores {
 		inscricao.setCpf(cpf);
 
 		inscricao.setNumeroInscricao(i++);
-		System.out.println("Informe as letras da Camiseta: Ex: 1. P, 2. M, 3. G, 4. GG, 5. XG e 6. XGG.");
-		Integer camisetaEscolhida = scan.nextInt();
-		NumeroCamiseta cursoEscolhido = whatCurso(camisetaEscolhida);
-		inscricao.setNumeroCamiseta(cursoEscolhido);
+		System.out.println("Informe o numero da Camiseta: Ex: 1. P, 2. M, 3. G, 4. GG, 5. XG e 6. XGG.");
+		Integer camiseta = scan.nextInt();
+		NumeroCamiseta camisetaEscolhida = whatCamiseta(camiseta);
+		inscricao.setNumeroCamiseta(camisetaEscolhida);
 		
 		System.out.println("Informe a prova: Ex: 1. Kids, 2. Caminhada_5K, 3. Corrida_5K, 4. Corrida_10K, 5. Corrida_21K");
 		Integer provaEscolhida = scan.nextInt();
@@ -46,30 +46,33 @@ public class InscricoesCorredores {
 		obterLinha();
 	}
 	
-	private NumeroCamiseta whatCurso(Integer camisetaEscolhida) {
-		return camisetaEscolhida == 1 ? NumeroCamiseta.P : 
-			camisetaEscolhida == 2 ? NumeroCamiseta.M :
-			camisetaEscolhida == 3 ? NumeroCamiseta.G :
-			camisetaEscolhida == 4 ? NumeroCamiseta.GG :
-			camisetaEscolhida == 5 ? NumeroCamiseta.XG : 
-			NumeroCamiseta.XGG;
-	}
-	
-	private OpcaoProva whatProva(Integer provaEscolhida) {
-		return provaEscolhida == 1 ? OpcaoProva.Kids : 
-			provaEscolhida == 2 ? OpcaoProva.Caminhada_5K :
-			provaEscolhida == 3 ? OpcaoProva.Corrida_5K :
-			provaEscolhida == 4 ? OpcaoProva.Corrida_10K :
-			OpcaoProva.Corrida_21K;
-	}
-	
 	// Opção 2 - Editar Inscrição
 	public void editar() {
 		System.out.println("Informe o numero da Inscrição: ");
 		Integer numero = scan.nextInt();
 		InscricaoCorredor inscricaoValidada = validarInscricao(numero);
 		if(inscricaoValidada != null) {
-			System.out.println("Editado");
+			System.out.println("Nome: ");
+			String nome = scan.next();
+			inscricaoValidada.setNome(nome);
+			
+			System.out.println("CPF: ");
+			String cpf = scan.next();
+			inscricaoValidada.setCpf(cpf);
+			
+			System.out.println("Numero da Camiseta: Ex: 1. P, 2. M, 3. G, 4. GG, 5. XG e 6. XGG.");
+			Integer camiseta = scan.nextInt();
+			NumeroCamiseta camisetaEscolhida = whatCamiseta(camiseta);
+			inscricaoValidada.setNumeroCamiseta(camisetaEscolhida);
+			
+			System.out.println("Informe a prova: Ex: 1. Kids, 2. Caminhada_5K, 3. Corrida_5K, 4. Corrida_10K, 5. Corrida_21K");
+			Integer provaEscolhida = scan.nextInt();
+			OpcaoProva provaEscolhido = whatProva(provaEscolhida);
+			inscricaoValidada.setOpcao(provaEscolhido);
+			
+			obterLinha();
+			System.out.println("Dados editados com sucesso!!");
+			obterLinha();
 		}else {
 			obterLinha();
 			System.out.println("Numero de inscrição incorreto, por favor informe corretamente!!");
@@ -89,5 +92,22 @@ public class InscricoesCorredores {
 	
 	public void obterLinha() {
 		System.out.println("==========================================");
+	}
+	
+	private NumeroCamiseta whatCamiseta(Integer camisetaEscolhida) {
+		return camisetaEscolhida == 1 ? NumeroCamiseta.P : 
+			camisetaEscolhida == 2 ? NumeroCamiseta.M :
+			camisetaEscolhida == 3 ? NumeroCamiseta.G :
+			camisetaEscolhida == 4 ? NumeroCamiseta.GG :
+			camisetaEscolhida == 5 ? NumeroCamiseta.XG : 
+			NumeroCamiseta.XGG;
+	}
+	
+	private OpcaoProva whatProva(Integer provaEscolhida) {
+		return provaEscolhida == 1 ? OpcaoProva.Kids : 
+			provaEscolhida == 2 ? OpcaoProva.Caminhada_5K :
+			provaEscolhida == 3 ? OpcaoProva.Corrida_5K :
+			provaEscolhida == 4 ? OpcaoProva.Corrida_10K :
+			OpcaoProva.Corrida_21K;
 	}
 }
